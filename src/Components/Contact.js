1b271fa61a5ed3c6496a369cace560fb07f34d15
mehraTushar <tushar78628@gmail.com>
2023-08-24 23:52:16 +0530

    update contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,12 @@
 const Contact = () => {
   return (
     <>
-      <div id="main">
+      <div
+        id="main"
+        className="h-[calc(100vh-150px)] flex justify-center items-center"
+      >
         <div className="fof">
-          <h1>This is Contact Page</h1>
+          <h1 className="text-5xl inline-block">This is Contact Page</h1>
         </div>
       </div>
     </>
