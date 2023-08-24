99e6335b830a82d01899008190d0c7692fa4833e
mehraTushar <tushar78628@gmail.com>
2023-08-24 23:52:03 +0530

    update about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,12 @@
 const About = () => {
   return (
     <>
-      <div id="main">
+      <div
+        id="main"
+        className="h-[calc(100vh-150px)] flex justify-center items-center"
+      >
         <div className="fof">
-          <h1>This is About Page</h1>
+          <h1 className=" text-5xl inline-block">This is About Page</h1>
         </div>
       </div>
     </>
