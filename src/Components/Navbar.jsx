b6018582708b0e30ec5b105e81b93f4667a379f7
Tushar Mehra <70595562+mehraTushar@users.noreply.github.com>
2024-08-10 10:21:43 +0530

    implement home page login logout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,11 +3,20 @@ import { useSelector } from 'react-redux';
 import Logo from './Logo';
 import { NavbarLinksArr } from '../config';
 import ThemeToggle from './ThemeToggle';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const Navbar = () => {
   const cartItemCount = useSelector((state) => {
     return state.Resturant.cartItems?.reduce((acc, currVal) => acc + currVal.Quantity, 0);
   });
+  const [isLoggedIn, setisLoggedIn] = useState(useLocalStorage('loggedInUser') ?? false);
+  function logoutFn() {
+    useLocalStorage('loggedInUser', null, true);
+    setisLoggedIn(false);
+  }
 
   return (
     <nav className="Navbar mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 bg-white dark:bg-black">
@@ -48,6 +57,19 @@ const Navbar = () => {
             )
           )}
           <ThemeToggle />
+          {isLoggedIn ? (
+            <button type="button" className="text-center" title="Logout" onClick={logoutFn}>
+              <FontAwesomeIcon icon={faRightToBracket} className="px-1 text-xl " />
+            </button>
+          ) : (
+            <Link
+              to="/login"
+              type="button"
+              className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Login
+            </Link>
+          )}
         </ul>
       </div>
     </nav>
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,5 @@
-export const useLocalStorage = (value, setValue) => {
+export const useLocalStorage = (value, setValue, isdeleted = false) => {
+  if (isdeleted) return localStorage.removeItem(value);
   if (setValue === undefined) return localStorage.getItem(value);
   if (typeof setValue === 'string') localStorage.setItem(value, setValue);
   else localStorage.setItem(value, JSON.stringify(setValue));
